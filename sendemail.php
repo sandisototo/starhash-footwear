db70e4104dca93c391639823352073d02a23f5a8
Sandiso <sandisototo@gmail.com>
2016-06-15 14:38:15 +0200

    4 Pages done

---
+++ sendemail.php
@@ -0,0 +1,18 @@
+<?php
+$name       = @trim(stripslashes($_POST['name']));
+$from       = @trim(stripslashes($_POST['emaild']));
+$subject    = @trim(stripslashes($_POST['subject']));
+
+$message    = $name." says: \r\n".@trim(stripslashes($_POST['message']));
+$to   		= 'sandisototo@gmail.com';//replace with your email
+
+$headers =  'MIME-Version: 1.0' . "\r\n";
+$headers .= "Content-type: text/plain; charset=iso-8859-1 ". "\r\n";
+$headers .= "From: Website Notification <{$from}>". "\r\n";
+$headers .= "Reply-To: <{$from}> ". "\r\n ";
+$headers .= "Subject: {$subject} ". "\r\n ";
+$headers .= "X-Mailer: PHP/".phpversion();
+
+mail($to, $subject, $message, $headers);
+
+die("Email sent to: ".$to);
